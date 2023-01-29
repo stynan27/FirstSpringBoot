05ac5a8338d3762393e9ae12397f94f52baedbcb
Seamus Tynan <32597881+stynan27@users.noreply.github.com>
2023-01-29 15:51:15 -0500

    Add files via upload

---
+++ learn-spring-boot/src/test/java/com/seamus/springboot/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamus.springboot.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
